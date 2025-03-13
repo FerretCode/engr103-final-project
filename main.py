8067a61479324e637950e56cef61c7d73a13ec85
ferretcode <eggertet@oregonstate.edu>
2025-03-12 21:56:50 -0700

    update formatting & add docs

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from typing import Union
 import csv
+import os
 
 MAX_FILENAME_CHARS = 255
 
 
-# TODO: do we want to add more file validation? it would add more complexity that probably isn't required for this project
 def validate_filename(filename: str) -> bool:
+    '''
+        Validates a filename. Ensures that it does not:
+            - Include a '.'
+            - Exceed the maximum characters. 255 by default
+    '''
+
     # no dots, plus we do not want the user providing the file extension
     if "." in filename:
         return False
@@ -16,22 +22,67 @@ def validate_filename(filename: str) -> bool:
 
     return True
 
+
 def validate_operation(operation: str) -> bool:
+    '''
+        Ensures a given operation is in the valid list of operations
+    '''
     return operation in operations
 
+
 def validate_budget(budget: str) -> bool:
+    '''
+        Ensures a given user input is a valid budget 
+    '''
     try:
         float(budget)
     except:
         print("Not a valid numerical budget")
         return False
-    
+
     if float(budget) < 0:
         print("Budget must be nonnegative")
         return False
     return True
 
-#should operations and categories not be in all caps, since we're using them as constants?
+
+def create_budget(budget: str, filename: str):
+    '''
+        Creates a budget CSV file based on a total budget amount
+            - Parses the user budget
+            - Calculates the % of USD for each category based on the category breakdown
+            - Writes the budget to a CSV file
+            - Outputs the budget
+    '''
+    filename = filename + ".csv"
+
+    while not validate_budget(budget):
+        budget = input()
+    budget = float(budget)
+
+    c = []
+    for entry in categories:
+        c.append((entry[0], budget * entry[1]))
+
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(c)
+
+    for entry in c:
+        print(f"{entry[0]}: ${entry[1]}")
+
+    file.close()
+
+
+def file_exists(filename: str) -> bool:
+    '''
+        Checks if a file exists
+    '''
+    filename = filename + ".csv"
+    return os.path.isfile(filename)
+
+
+# should operations and categories not be in all caps, since we're using them as constants?
 operations = [
     "new",
     "modify",
@@ -49,68 +100,49 @@ categories = [
     ("Clothing", 0.05),
 ]
 
-def create_budget(budget: str, filename: str):
-    filename = filename + ".csv"
-
-    while not validate_budget(budget):
-        budget = input()
-    budget = float(budget)
-    
-    c = []
-    for entry in categories:
-        c.append((entry[0], budget * entry[1]))
-
-    with open(filename, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(c)
-    
-    for entry in c:
-        print(entry[0], ": $", entry[1])
-
-    file.close()
-
-def validate_path(filename: str) -> bool:
-    filename = filename + ".csv"
-    try:
-        with open(filename, 'r') as file:
-            return True
-    except FileNotFoundError:
-        return False
-
 """
 For all operation functions, they will return either:
     - None (nothing) if the operation is completed successfully
     - String of the error message if there was an error
 """
 
+
 def new(filename: str) -> Union[None, str]:
-    if not validate_path(filename):
+    '''
+        Create a new budget based on a filename and user provided budget
+    '''
+    if file_exists(filename):
         return "File Already Exists, Use Modify Instead"
-    
+
     budget = input("Enter your budget: ")
-    create_budget(budget)
+    create_budget(budget, filename)
     return None
 
 
 def modify(filename: str) -> Union[None, str]:
-    if validate_path(filename):
+    '''
+        Modify an existing budget to take a new total budget and update an existing budget
+    '''
+    if not file_exists(filename):
         return "File Does Not Exists, Use New Instead"
-    
+
     budget = input("Enter your budget: ")
-    create_budget(budget)
+    create_budget(budget, filename)
     return None
 
+
 def view(filename: str) -> Union[None, str]:
+    '''
+        View an existing budget
+    '''
     filename = filename + ".csv"
-    if not validate_path(filename):
+    if file_exists(filename):
         return "File Does Not Exist, Use New Instead"
-    
+
     with open(filename, 'r') as file:
         content = file.read()
         print(content)
     return None
-
-
 
 
 def main():
@@ -131,19 +163,19 @@ def main():
             continue
 
         if operation == "new":
-            res = new()
+            res = new(filename)
             if res is not None:
                 print(res)
                 continue
 
         if operation == "modify":
-            res = modify()
+            res = modify(filename)
             if res is not None:
                 print(res)
                 continue
 
         if operation == "view":
-            res = view()
+            res = view(filename)
             if res is not None:
                 print(res)
                 continue
